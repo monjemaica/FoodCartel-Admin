2488d811bfae572fb83a2973983ee816ca2d6b6b
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-07-27 16:06:39 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AdminDashboard from './components/AdminDashboard'
+
+function App() {
+
+  return (
+    <>
+      <AdminDashboard></AdminDashboard>
+    </>
+  )
+}
+
+export default App
